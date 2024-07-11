66c5143d96f16c37908ffd057bedcf655d84f84f
zahiakp <mohammedzahiayikkarappadi@gmail.com>
2024-07-11 22:40:12 +0530

    dfsddfs

---
+++ app/(admin)/admin/config/page.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from '@/components/layouts/AdminLayout'
+import React from 'react'
+
+function page() {
+  return (
+    <AdminLayout>
+      config
+    </AdminLayout>
+  )
+}
+
+export default page
