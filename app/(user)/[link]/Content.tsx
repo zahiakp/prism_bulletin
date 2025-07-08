adc4dbd4a1c7a18dd2fd025e2c1304ea2f137da5
zahiakp <121743451+zahiakp@users.noreply.github.com>
2025-07-08 17:06:47 +0530

    maintenance

--- app/(user)/[link]/Content.tsx
+++ app/(user)/[link]/Content.tsx
@@ -19,15 +19,17 @@ import React from "react";
 
 
 function Content({ data }: { data: any }) {
-  // const data = data[0];
+  if (!data) {
+    return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  }
 
   return (
     <main className="flex flex-col ">
-      <h6 className="text-5xl font-bold leading-[60px]">{data.title}</h6>
+      <h6 className="text-5xl font-bold leading-[60px]">{data?.title}</h6>
       <span className="text-blue-600">{getRelativeTime(data?.date)}</span>
       {data?.image!="" && <div className="w-full h-[450px] overflow-hidden rounded-2xl my-7">
         <img
-          src={`${ROOT_URL}uploads/news/${data.image}`}
+          src={`${ROOT_URL}uploads/news/${data?.image}`}
           alt=""
           className="w-full h-full object-cover"
         />
@@ -37,7 +39,7 @@ function Content({ data }: { data: any }) {
                         className="text-justify leading-relaxed"
                         dangerouslySetInnerHTML={{ __html: data?.body }}
                       />
-      <div className="flex gap-2 my-10 flex-wrap">{StringtoArray(data.tags).map((item:any,index:number)=><p key={index} className="p-[6px] px-7 bg-zinc-100 rounded-3xl">{item}</p>)}</div>
+      <div className="flex gap-2 my-10 flex-wrap">{StringtoArray(data?.tags).map((item:any,index:number)=><p key={index} className="p-[6px] px-7 bg-zinc-100 rounded-3xl">{item}</p>)}</div>
     </main>
   );
 }
--- app/(user)/[link]/SharetoWhatsapp.tsx
+++ app/(user)/[link]/SharetoWhatsapp.tsx
@@ -9,13 +9,13 @@ interface PostData {
 
 export const shareToWhatsApp = (post: PostData) => {
     // Limit body text to 500 characters and remove HTML tags
-    const bodyText = post.body.replace(/<[^>]+>/g, '').slice(0, 500);
+    const bodyText = post?.body?.replace(/<[^>]+>/g, '').slice(0, 500);
   
     // Construct the post URL
-    const postUrl = `https://bulletin.prismonline.org/${post.url}_${encodeId(post.id)}`;
+    const postUrl = `https://bulletin.prismonline.org/${post?.url}_${encodeId(post?.id)}`;
   
     // WhatsApp sharing URL with properly constructed body and post URL
-    const message = `*${post.title}*\n\n${bodyText}\n\nRead more:\n${postUrl}`;
+    const message = `*${post?.title}*\n\n${bodyText}\n\nRead more:\n${postUrl}`;
 
     // Correctly encode the message for WhatsApp URL
     const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}`;
